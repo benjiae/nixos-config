9bbca752d91182fa45c11085adec851d7634a19b
benjae <benjaminang2009@gmail.com>
2024-09-15 20:36:32 -0300

    <- clueless

--- home/configs/user-bins.nix
+++ home/configs/user-bins.nix
@@ -2,6 +2,6 @@
 
 {
 	# Nix scripts
-	home.file."${config.home.homeDirectory}/.local/bin/commit-nix".source = ../../commit-nix
-	home.file."${config.home.homeDirectory}/.local/bin/update".source = ../../update
+	home.file.".local/bin/commit-nix".source = ../../commit-nix
+	home.file.".local/bin/update".source = ../../update
 }
