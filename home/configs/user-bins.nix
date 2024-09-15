9f822b973cfe2714c1448445bbc46e100f7c589f
benjae <benjaminang2009@gmail.com>
2024-09-15 20:45:49 -0300

    semicolons (only that hopefully9

--- home/configs/user-bins.nix
+++ home/configs/user-bins.nix
@@ -2,6 +2,6 @@
 
 {
 	# Nix scripts
-	home.file.".local/bin/commit-nix".source = ../../commit-nix
-	home.file.".local/bin/update".source = ../../update
+	home.file.".local/bin/commit-nix".source = ../../commit-nix;
+	home.file.".local/bin/update".source = ../../update;
 }
