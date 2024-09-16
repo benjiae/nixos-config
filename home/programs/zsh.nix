3b722151f93a6315b8c71261bee23b43d046e0cc
benjae <benjaminang2009@gmail.com>
2024-09-15 21:39:14 -0300

    big update

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -18,14 +18,13 @@
 		plugins = [
 			{
 				name = "pure";
-				src = pkgs.fetchFromGithub {
+				src = pkgs.fetchFromGitHub {
 					owner = "sindresorhus";
 					repo = "pure";
-					rev = "v1.23.0"
+					rev = "v1.23.0";
 					sha256 = "1jcb5cg1539iy89vm9d59g8lnp3dm0yv88mmlhkp9zwx3bihwr06";
 				};
 			}
 		];
-
 	};
 }
